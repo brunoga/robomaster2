0e47fcbd87daf99802e701cee0c9c27185b6750f
Bruno Albuquerque <bga@gmail.com>
2023-08-11 16:24:00 -0400

    Add command controller.

---
+++ internal/generic_controller.go
@@ -0,0 +1,53 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/brunoga/robomaster2/internal/event"
+	"github.com/brunoga/robomaster2/internal/unitybridge"
+)
+
+type GenericController struct {
+	eventHandler unitybridge.EventCallbackHandler
+
+	eventHandlerIndexMap map[event.Type]struct{}
+}
+
+func NewGenericController(eventHandler unitybridge.EventCallbackHandler) *GenericController {
+	return &GenericController{
+		eventHandler,
+		make(map[event.Type]struct{}),
+	}
+}
+
+func (g *GenericController) StartControllingEvent(typ event.Type) error {
+	b := unitybridge.Get()
+
+	_, ok := g.eventHandlerIndexMap[typ]
+	if ok {
+		return fmt.Errorf("event type %s is already being controlled",
+			event.TypeName(typ))
+	}
+
+	b.SetEventCallback(int64(typ), g.eventHandler)
+
+	g.eventHandlerIndexMap[typ] = struct{}{}
+
+	return nil
+}
+
+func (g *GenericController) StopControllingEvent(typ event.Type) error {
+	b := unitybridge.Get()
+
+	_, ok := g.eventHandlerIndexMap[typ]
+	if !ok {
+		return fmt.Errorf("event type %s is not being controlled",
+			event.TypeName(typ))
+	}
+
+	b.SetEventCallback(int64(typ), nil)
+
+	delete(g.eventHandlerIndexMap, typ)
+
+	return nil
+}
