35116f70bee2770a5d28dfa0fbf94fa745931fe2
Bruno Albuquerque <bga@gmail.com>
2023-10-31 15:55:40 -0400

    Re-add libraries and other files.

---
+++ internal/robot/service/unitybridge/example/example.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/brunoga/robomaster2/internal/unitybridge"
+
+func main() {
+	ub := unitybridge.Get()
+
+	ub.Create("Robomaster", true, "./log")
+	defer ub.Destroy()
+
+	if !ub.Initialize() {
+		panic("UnityBridge failed to initialize")
+	}
+	defer ub.Uninitialize()
+}
